4458c0883471dd9a431ed572c8eeb93114b909b5
Marcus Rückert <darix@nordisch.org>
2015-03-12 16:33:29 +0100

    merge with latest copy from rpm before the rewrite

--- gem_install.sh
+++ gem_install.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 set -e
+shopt -s nullglob
 
 # options may be followed by one colon to indicate they have a required argument
-if ! options=$(getopt -o dEf -l ignore-dependencies,force,no-rdoc,rdoc,no-ri,ri,env-shebang,no-env-shebang,symlink-binaries,default-gem:,build-root:,doc-files:,gem-name:,gem-version:,gem-suffix: -- "$@")
+if ! options=$(getopt -o dEf -l ignore-dependencies,force,no-rdoc,rdoc,no-ri,ri,env-shebang,no-env-shebang,symlink-binaries,default-gem:,build-root:,doc-files:,gem-name:,gem-version:,gem-suffix:,gem-binary: -- "$@")
 then
     # something went wrong, getopt will put out an error message for us
     exit 1
@@ -11,7 +12,7 @@ fi
 
 eval set -- "$options"
 
-gem_binary="/usr/bin/gem.*"
+gem_binary="/usr/bin/gem.* /usr/bin/gem[0-9].[0-9]"
 defaultgem=
 gemfile=
 otheropts=
@@ -29,7 +30,7 @@ while [ $# -gt 0 ]
 do
     case $1 in
     --default-gem) defaultgem=$2 ; shift;;
-    --gem-binary) gem_binary="$2" ; shift;;
+    --gem-binary) echo "Ignored --gem-binary option" >&2 ; shift;;
     --doc-files) docfiles="$2" ; shift;;
     --gem-name) gemname="$2" ; shift;;
     --gem-version) gemversion="$2" ; shift;;
@@ -59,15 +60,23 @@ mkdir -p "${RPM_BUILD_ROOT}$docdir"
 for gem in $gem_binary ; do
   $gem install --verbose --local $otheropts
   # get the ruby interpreter
-  ruby="${gem#/usr/bin/gem.}"
-  rpmname="${ruby}-rubygem-${gemname}${gemsuffix:+$gemsuffix}"
+  ruby="${gem#/usr/bin/gem}"
+  if [[ $ruby == [0-9]* ]] ; then
+    destruby=".ruby$ruby"
+  else
+    destruby="$ruby"
+  fi
+  rpmname="${destruby#.}-rubygem-${gemname}${gemsuffix:+$gemsuffix}"
   if test -d $RPM_BUILD_ROOT/usr/bin; then
     pushd $RPM_BUILD_ROOT/usr/bin
     if [ "x$symlinkbinaries" = "xtrue" ] ; then
       for i in *$ruby ; do
-        unversioned="${i%.$ruby}"
-        mv -v ${i} ${i}-${gemversion}
-        ruby -p -i -e "\$_.gsub!(/>= 0/, '= $gemversion')" ${i}-${gemversion}
+        unversioned="${i%$ruby}"
+        fullyversioned="$unversioned$destruby-${gemversion}"
+        rubyversioned="$unversioned$destruby"
+        gemversioned="$unversioned-$gemversion"
+        mv -v ${i} $fullyversioned
+        perl -p -i -e "s/>= 0/= $gemversion/" $fullyversioned
         if [ ! -L ${RPM_BUILD_ROOT}${ua_dir}/${unversioned} ] ; then
           ln -sv ${unversioned} ${RPM_BUILD_ROOT}${ua_dir}/${unversioned}
         fi
@@ -77,24 +86,24 @@ for gem in $gem_binary ; do
         fi
 
         # ruby versioned
-        if [ ! -L ${RPM_BUILD_ROOT}${ua_dir}/${i} ] ; then
-          ln -sv ${i} ${RPM_BUILD_ROOT}${ua_dir}/$i
+        if [ ! -L ${RPM_BUILD_ROOT}${ua_dir}/$rubyversioned ] ; then
+          ln -sv ${rubyversioned} ${RPM_BUILD_ROOT}${ua_dir}/$rubyversioned
         fi
-        if [ ! -L $i ] ; then
-          ln -sv ${ua_dir}/${i} $i
+        if [ ! -L $rubyversioned ] ; then
+          ln -sv ${ua_dir}/${rubyversioned} $rubyversioned
         fi
         # gem versioned
-        if [ ! -L ${RPM_BUILD_ROOT}${ua_dir}/$unversioned-$gemversion ] ; then
-          ln -sv ${unversioned}-$gemversion ${RPM_BUILD_ROOT}${ua_dir}/$unversioned-$gemversion
+        if [ ! -L ${RPM_BUILD_ROOT}${ua_dir}/$gemversioned ] ; then
+          ln -sv $gemversioned ${RPM_BUILD_ROOT}${ua_dir}/$gemversioned
         fi
-        if [ ! -L $unversioned-$gemversion ] ; then
-          ln -sv ${ua_dir}/${unversioned}-$gemversion $unversioned-$gemversion
+        if [ ! -L $gemversioned ] ; then
+          ln -sv ${ua_dir}/$gemversioned $gemversioned
         fi
       done ;
     else
       for i in *$ruby ; do
         # lets undo the format-executable to avoid breaking more spec files
-        unversioned="${i%.$ruby}"
+        unversioned="${i%$ruby}"
         mv -v $i $unversioned 
       done
     fi
@@ -110,7 +119,7 @@ for gem in $gem_binary ; do
   if [ -d "$buildroot" ]; then
     find ${buildroot}${gemdir} -type f -perm /u+x | while read file; do
       # TODO: scripts in ruby/1.9.1 should call ruby1.9 for consistency
-      sed -i -e "s,^#!/usr/bin/env ruby,#!/usr/bin/ruby,; s,^#! *[^ ]*/ruby\S\+,#!/usr/bin/ruby.$ruby," "$file"
+      perl -p -i -e "s,^#!/usr/bin/env ruby,#!/usr/bin/ruby,; s,^#! *[^ ]*/ruby\S*,#!/usr/bin/ruby$ruby," "$file"
     done
     # some windows made gems are broken
     find $buildroot -ls
